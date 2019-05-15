c2d1c55ca8fb4d955bc527a7c6fe147916db2770
Neeraj Anturkar <neeraj@kisan.net>
2019-05-15 22:34:26 +0200

    New : Initial Commit

---
+++ code/include/Constants.php
@@ -0,0 +1,15 @@
+<?php
+//Constants to connect with the database STARTS
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', 'Anturkar@05');
+// define('DB_HOST', 'localhost');
+// define('DB_NAME', 'greencloudservice_local_k18');
+// //Constants to connect with the database ENDS
+
+// //Constants to connect with the database STARTS
+// define('REMOTE_DB_USERNAME', 'root');
+// define('REMOTE_DB_PASSWORD', 'Anturkar@05');
+// define('REMOTE_DB_HOST', 'localhost');
+// define('REMOTE_DB_NAME', 'greencloudservice_local_k18');
+// //Constants to connect with the database ENDS
+?>
