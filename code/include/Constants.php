bcd078e321681c4434f3829a51bbd76e43a1f6b1
Neeraj Anturkar <neeraj@kisan.net>
2019-05-26 04:36:29 +0200

    New : Email Notification / Check-Check-out integration

--- code/include/Constants.php
+++ code/include/Constants.php
@@ -4,6 +4,7 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'Anturkar@05');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'bookworm_db');
+define('MAIL_API_KEY','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');
 
 // //Constants to connect with the database ENDS
 
